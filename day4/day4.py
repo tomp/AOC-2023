235f450edd4d30d1c63de07f7072eb238436ede0
Tom Pollard <pollard.tom@gmail.com>
2023-12-04 01:00:09 -0500

    day 4 solution

---
+++ day4/day4.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+#
+#  Advent of Code 2023 - Day 4
+#
+from typing import Sequence, Union, Optional, Any, Dict, List, Tuple
+from pathlib import Path
+from collections import defaultdict
+from dataclasses import dataclass
+from pprint import pprint
+import math
+import re
+
+INPUTFILE = "input.txt"
+
+SAMPLE_CASES = [
+    (
+        """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """,
+        13
+    ),
+]
+
+SAMPLE_CASES2 = [
+    (
+        """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """,
+        30
+    ),
+]
+
+
+Lines = Sequence[str]
+Sections = Sequence[Lines]
+
+# Utility functions
+
+def load_input(infile: str, strip=True, blank_lines=False) -> Lines:
+    return load_text(Path(infile).read_text())
+
+def load_text(text: str, strip=True, blank_lines=False) -> Lines:
+    if strip:
+        lines = [line.strip() for line in text.strip("\n").split("\n")]
+    else:
+        lines = [line for line in text.strip("\n").split("\n")]
+    if blank_lines:
+        return lines
+    return [line for line in lines if line.strip()]
+
+def parse_sections(lines: Lines) -> Sections:
+    result = []
+    sect = []
+    for line in lines:
+        if not line.strip():
+            if sect:
+                result.append(sect)
+            sect = []
+        else:
+            sect.append(line)
+    if sect:
+        result.append(sect)
+    return result
+
+
+# Solution
+
+CARD_RE = re.compile(r"Card\s+(\d+):\s+(\d[\d ]+\d)\s+\|\s+(\d[\d ]+\d)\s*$")
+
+
+def parse_lines(lines):
+    result = []
+    for line in lines:
+        m = CARD_RE.match(line)
+        if m:
+            num = int(m.group(1))
+            winners = m.group(2).split()
+            mine = m.group(3).split()
+            common = len(set(winners) & set(mine))
+            result.append((num, common))
+    return result
+
+
+def solve2(lines: Lines) -> int:
+    """Solve the problem."""
+    cards = parse_lines(lines)
+    count = {num: 1 for num, _ in cards}
+    for num, common in cards:
+        for i in range(num+1, min(len(cards)+1, num+common+1)):
+            count[i] += count[num]
+    return sum(count.values())
+
+
+def solve(lines: Lines) -> int:
+    """Solve the problem."""
+    result = 0
+    cards = parse_lines(lines)
+    for num, common in cards:
+        result += int(pow(2, common-1))
+    return result
+
+
+# PART 1
+
+def example1() -> None:
+    """Run example for problem with input arguments."""
+    print("EXAMPLE 1:")
+    for text, expected in SAMPLE_CASES:
+        lines = load_text(text)
+        result = solve(lines)
+        print(f"'{text}' -> {result} (expected {expected})")
+        assert result == expected
+    print("= " * 32)
+
+def part1(lines: Lines) -> None:
+    print("PART 1:")
+    result = solve(lines)
+    print(f"result is {result}")
+    assert result == 20117
+    print("= " * 32)
+
+
+# PART 2
+
+def example2() -> None:
+    """Run example for problem with input arguments."""
+    print("EXAMPLE 2:")
+    for text, expected in SAMPLE_CASES2:
+        lines = load_text(text)
+        result = solve2(lines)
+        print(f"'{text}' -> {result} (expected {expected})")
+        assert result == expected
+    print("= " * 32)
+
+def part2(lines: Lines) -> None:
+    print("PART 2:")
+    result = solve2(lines)
+    print(f"result is {result}")
+    assert result == 13768818
+    print("= " * 32)
+
+
+if __name__ == "__main__":
+    example1()
+    input_lines = load_input(INPUTFILE)
+    part1(input_lines)
+    example2()
+    part2(input_lines)
